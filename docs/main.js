7646560448322c00351216175b10c55c2d5ce29a
willsims14 <willsims14@gmail.com>
2021-10-03 11:35:24 -0500

    Added inventory

---
+++ docs/main.js
@@ -0,0 +1,26 @@
+$(document).ready(function () {
+  // your code here
+  // document.addEventListener('DOMContentLoaded', function() {
+  //   var elems = document.querySelectorAll('.carousel');
+  //   var instances = M.Carousel.init(elems, options);
+  // });
+
+  // // Or with jQuery
+  // $(document).ready(function(){
+  //   $('.carousel').carousel();
+  // });
+
+  // var instance = M.Carousel.init({
+  //   fullWidth: true,
+  //   indicators: true
+  // });
+
+  // // Or with jQuery
+
+  $('.carousel.carousel-slider').carousel({
+    fullWidth: true,
+    indicators: true
+  });
+    M.AutoInit();
+
+});
